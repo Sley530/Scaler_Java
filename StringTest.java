95852e35174cd0acb787eaf6f5b21c3119e0efed
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-02-20 22:36:33 -0500

    Commit

---
+++ StringTest.java
@@ -0,0 +1,30 @@
+/**
+ *
+ */
+package day4;
+
+/**
+ * This is a simple program to display the
+ * character in a string.
+ *
+ * @author sleys
+ *
+ */
+public class StringTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		String me = "I am a computer engineer";
+
+		System.out.print(me.charAt(20));
+		System.out.print(me.charAt(19));
+		System.out.print(me.charAt(7));
+		System.out.println(me.charAt(22) + "!!!");
+		System.out.println(me.length());
+	}
+
+
+}
